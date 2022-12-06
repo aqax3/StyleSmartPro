d6b9b6ee7ec96030822355ae599431327306c4e1
lenabojc81 <bojc.lena@gmail.com>
2022-12-06 16:39:40 +0100

    implementacija GET, PUT, POST, DELETE metod #SSP-17 DONE Time 2h

--- src/main/java/com/StyleSmartPro/StyleSmartPro/models/Storitev.java
+++ src/main/java/com/StyleSmartPro/StyleSmartPro/models/Storitev.java
@@ -1,9 +1,49 @@
 package com.StyleSmartPro.StyleSmartPro.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Storitev {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id", nullable = false)
+	private Long id;
 
 	private String ime;
 	private int priblizenCasTrajanja;
 	private String spol;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public int getPriblizenCasTrajanja() {
+		return priblizenCasTrajanja;
+	}
+
+	public void setPriblizenCasTrajanja(int priblizenCasTrajanja) {
+		this.priblizenCasTrajanja = priblizenCasTrajanja;
+	}
+
+	public String getSpol() {
+		return spol;
+	}
+
+	public void setSpol(String spol) {
+		this.spol = spol;
+	}
 
 }
